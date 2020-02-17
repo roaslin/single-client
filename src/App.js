d9be02879d5b3f79c6801f9dc0fb567e46627daa
raulcodurance <raul.mordillo@codurance.com>
2020-02-17 15:52:41 +0100

    edited text

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React edited!
+          Learn React 17 February!
         </a>
       </header>
     </div>
